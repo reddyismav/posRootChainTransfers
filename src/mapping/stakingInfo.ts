1648eb7992b5cbafa4bd7e4acedd3143615da8ef
Vamsi <vamsireddy9672@gmail.com>
2021-05-31 15:39:12 +0530

    stake updates

---
+++ src/mapping/stakingInfo.ts
@@ -0,0 +1,15 @@
+import { UpdatedStake } from '../../generated/schema'
+import { StakeUpdate } from '../../generated/StakingInfo/StakingInfo'
+
+export function handleStakeUpdate(event: StakeUpdate): void {
+    let id = event.transaction.hash.toHex();
+    let updatedStake = new UpdatedStake(id);
+
+    updatedStake.amount = event.params.newAmount;
+    updatedStake.nonce = event.params.nonce;
+    updatedStake.validatorId = event.params.validatorId;
+    updatedStake.blockNumber = event.block.number;
+    updatedStake.transactionHash = event.transaction.hash;
+    updatedStake.logIndex = event.logIndex;
+    updatedStake.transactionLogIndex = event.transactionLogIndex;
+}
